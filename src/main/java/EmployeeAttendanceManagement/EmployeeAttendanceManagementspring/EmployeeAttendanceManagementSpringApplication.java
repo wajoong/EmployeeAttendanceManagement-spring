3fc7de4d7a336d9b95eab6d4d94b7fa11cdb20e7
wajoong <kjuseong4121@naver.com>
2023-05-11 04:12:49 +0900

    init git

---
+++ src/main/java/EmployeeAttendanceManagement/EmployeeAttendanceManagementspring/EmployeeAttendanceManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package EmployeeAttendanceManagement.EmployeeAttendanceManagementspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceManagementSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EmployeeAttendanceManagement/EmployeeAttendanceManagementspring/EmployeeAttendanceManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeeAttendanceManagement.EmployeeAttendanceManagementspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAttendanceManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
