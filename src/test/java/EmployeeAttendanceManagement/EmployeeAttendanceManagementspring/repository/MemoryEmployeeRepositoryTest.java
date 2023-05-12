8b10e841b52d825bc30ed203555a2f8d76b05ba4
wajoong <kjuseong4121@naver.com>
2023-05-12 11:29:47 +0900

    git init

--- src/test/java/EmployeeAttendanceManagement/EmployeeAttendanceManagementspring/repository/MemoryEmployeeRepositoryTest.java
+++ src/test/java/EmployeeAttendanceManagement/EmployeeAttendanceManagementspring/repository/MemoryEmployeeRepositoryTest.java
@@ -4,6 +4,8 @@ import model.Employee;
 import org.junit.jupiter.api.Test;
 import repository.MemoryEmployeeRepository;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryEmployeeRepositoryTest {
@@ -24,7 +26,23 @@ class MemoryEmployeeRepositoryTest {
 
     @Test
     public void remove(){
+        Employee employee = new Employee();
+        employee.setStaffNumber(2019);
+        employee.setName("주원");
 
+        repository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setStaffNumber(2020);
+        employee1.setName("현우");
+
+        repository.save(employee1);
+
+        repository.remove(employee1);
+
+        List<Employee> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(1);
     }
 
 
